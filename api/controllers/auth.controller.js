c0c54bec54fdf0233930ed3f7a7551146fd8a30d
“Tsundo” <tenzintsundo@gmail.com>
2024-11-12 22:48:27 +0100

    create signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -25,7 +26,25 @@ export const signup = async (req, res, next) => {
     }
 };
 
-export const signin = (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
-}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        next(errorHandler(400, "All fields are required"))
+    }
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(400, "Invalid Password"));
+
+        const token = jwt.sign(
+            { id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "24h" }
+        )
+        const { password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie("access_token", token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
